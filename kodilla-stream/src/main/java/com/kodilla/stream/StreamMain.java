e54fbbc50c787d0ebd51054926ab75eee085e329
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2018-10-16 17:33:50 +0200

    Task 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -7,18 +8,14 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        PoemBeautifier exampleBeautifier = new PoemBeautifier();
+        exampleBeautifier.PoemBeautifier("abcd", text -> text.toUpperCase());
+        exampleBeautifier.PoemBeautifier("abcd", text -> text + " efgh");
+        exampleBeautifier.PoemBeautifier("abcd", text -> text.substring(0,2));
+        exampleBeautifier.PoemBeautifier("abcd", text -> "~~"+text+"~~");
+
+
     }
 }
