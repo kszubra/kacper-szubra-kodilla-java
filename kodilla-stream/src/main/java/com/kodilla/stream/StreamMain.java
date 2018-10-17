3f34421f1fb6fa81a574757f3e77157910990545
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2018-10-17 10:02:12 +0200

    update lesson

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -9,12 +10,14 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 public class StreamMain {
     public static void main(String[] args) {
 
-
-        PoemBeautifier exampleBeautifier = new PoemBeautifier();
+        /*PoemBeautifier exampleBeautifier = new PoemBeautifier();
         exampleBeautifier.PoemBeautifier("abcd", text -> text.toUpperCase());
         exampleBeautifier.PoemBeautifier("abcd", text -> text + " efgh");
         exampleBeautifier.PoemBeautifier("abcd", text -> text.substring(0,2));
-        exampleBeautifier.PoemBeautifier("abcd", text -> "~~"+text+"~~");
+        exampleBeautifier.PoemBeautifier("abcd", text -> "~~"+text+"~~"); */
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 
     }
