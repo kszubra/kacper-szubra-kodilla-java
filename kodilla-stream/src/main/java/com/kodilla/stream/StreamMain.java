ff09b099ab8075ebbe3896cb6061ba18e7feb806
unknown <kszubra@yahoo.com>
2018-10-20 19:42:48 +0200

    Task 7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,16 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.array.ArrayOperations;
+
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
+
     public static void main(String[] args) {
 
         Forum exampleForum = new Forum();
@@ -21,6 +23,8 @@ public class StreamMain {
         resultForumUsersMap.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
+
+
 
 
     }
