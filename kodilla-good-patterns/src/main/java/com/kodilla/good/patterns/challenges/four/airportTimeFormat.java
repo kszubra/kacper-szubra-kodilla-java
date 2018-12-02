ccb18eba242eeb9f0d7d4ef0a672c60123d20569
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2018-12-02 15:29:46 +0100

    Task 13.4

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/four/airportTimeFormat.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.challenges.four;
+
+import java.time.format.DateTimeFormatter;
+
+class airportTimeFormat {
+
+     static DateTimeFormatter AIRPORT_TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
+}
