a3a364410fd06841d089a2ee16ba51383a0ba5c2
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2018-12-06 23:16:59 +0100

    requested changes

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -12,33 +12,51 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CalculatorTestSuite {
 
+    private ApplicationContext context =
+            new AnnotationConfigApplicationContext("com.kodilla.spring");
+    private Calculator testCalculator = context.getBean(Calculator.class);
+
+    private double a = 10.0;
+    private double b = 2.0;
+
     @Test
-    public void testCalculations() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        Calculator testCalculator = context.getBean(Calculator.class);
-
-        double a = 10.0;
-        double b = 2.0;
-
+    public void testCalculationsAdd() {
         //When
         double expectedAdd = 12.0;
         double realAdd = testCalculator.add(a, b);
 
+        //Then
+        Assert.assertEquals(expectedAdd, realAdd, 0.001);
+    }
+
+    @Test
+    public void testCalculationsSub() {
+        //When
         double expectedSub = 8.0;
         double realSub = testCalculator.sub(a, b);
 
+        //Then
+        Assert.assertEquals(expectedSub, realSub, 0.001);
+    }
+
+    @Test
+    public void testCalculationsDiv() {
+        //When
         double expectedDiv = 5.0;
         double realDiv = testCalculator.div(a, b);
 
+        //Then
+        Assert.assertEquals(expectedDiv, realDiv, 0.001);
+
+    }
+
+    @Test
+    public void testCalculationsMul() {
+        //When
         double expectedMul = 20.0;
         double realMul = testCalculator.mul(a, b);
 
         //Then
-        Assert.assertEquals(expectedAdd, realAdd, 0.001);
-        Assert.assertEquals(expectedSub, realSub, 0.001);
-        Assert.assertEquals(expectedDiv, realDiv, 0.001);
         Assert.assertEquals(expectedMul, realMul, 0.001);
     }
 }
