39d38f6c79c3e5b3b8fc621c959b816ce53a6238
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-01-13 01:26:13 +0100

    removed unused query import

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/CompanyDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/CompanyDao.java
@@ -1,7 +1,6 @@
 package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Company;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
