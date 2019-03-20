cd077da58df871b6bfcbd833f4e036cdef95f830
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-20 13:09:24 +0100

    so far 320 lines to rewrite who offers more

---
+++ kodilla-patterns2/src/main/java/com/kodillapatterns2/facade/api/OrderFacade.java
@@ -0,0 +1,18 @@
+package com.kodillapatterns2.facade.api;
+
+import com.kodillapatterns2.facade.ShopService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class OrderFacade {
+    @Autowired
+    private ShopService shopService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
+
+
+
+
+}
