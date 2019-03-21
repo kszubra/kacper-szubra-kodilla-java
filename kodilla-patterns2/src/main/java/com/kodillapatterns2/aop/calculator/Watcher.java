08daf256d49a0ae8c052382223962574272f217c
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-21 20:22:38 +0100

    lesson update

--- kodilla-patterns2/src/main/java/com/kodillapatterns2/aop/calculator/Watcher.java
+++ kodilla-patterns2/src/main/java/com/kodillapatterns2/aop/calculator/Watcher.java
@@ -1,11 +1,15 @@
 package com.kodillapatterns2.aop.calculator;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Aspect
 @Component
@@ -31,8 +35,39 @@ public class Watcher {
      * execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..)) - metody factorial() klasy Calculator.
      */
 
-    @Before("execution(* com.kodillapatterns2.aop.calculator.Calculator.factorial(..))")
-    public void logEvent() {
-        LOGGER.info("Logging the event");
+    @Before("execution(* com.kodillapatterns2.aop.calculator.Calculator.factorial(..))" +
+    "&& args(theNumber) && target(object)")
+    public void logEvent(BigDecimal theNumber, Object object) { // wyrażenie args(...), które pozwala przekazać do wskazanej zmiennej argumenty jakich użyto do wywołania metody, na której załączany jest aspekt.
+        LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + theNumber);
     }
+
+    /**
+     * Adnotacja @Around przyjmuje takie same argumenty jak adnotacja @Before. Inna jest jednak konstrukcja samej metody:     *
+     * Metoda aspektu musi zwracać obiekt
+     * Metoda aspektu przyjmuje parametr ProceedingJoinPoint proceedingJoinPoint
+     *
+     *  proceedingJoinPoint reprezentuje "uchwyt" do przechwyconej metody. Obiekt ten posiada metodę proceed(), która powoduje uruchomienie przechwyconej metody.
+     *  Wynik jej wywołania należy zwrócić przy pomocy return.
+     *
+     *
+     */
+
+    @Around("execution(* com.kodillapatterns2.aop.calculator.Calculator.factorial(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try{
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Time consumed: " + (end - begin) + "[ms]");
+        } catch(Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+
+        return result;
+    }
+
+
+
 }
