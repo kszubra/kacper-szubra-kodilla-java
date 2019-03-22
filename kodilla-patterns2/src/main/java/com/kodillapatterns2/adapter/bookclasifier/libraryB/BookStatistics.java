63b10c09e76e3892e9dbd6fae2a966c81810c8cd
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-22 18:34:06 +0100

    Task 28.5

---
+++ kodilla-patterns2/src/main/java/com/kodillapatterns2/adapter/bookclasifier/libraryB/BookStatistics.java
@@ -0,0 +1,9 @@
+package com.kodillapatterns2.adapter.bookclasifier.libraryB;
+
+import java.util.Map;
+
+public interface BookStatistics {
+    int averagePublicationYear(Map<BookSignature, Book> books);
+    int medianPublicationYear(Map<BookSignature, Book> books);
+
+}
