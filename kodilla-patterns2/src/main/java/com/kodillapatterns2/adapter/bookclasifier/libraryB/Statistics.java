8e27943ac96cabbd2c10425a4c62a32af1837275
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-03-23 14:26:45 +0100

    better median calculation

--- kodilla-patterns2/src/main/java/com/kodillapatterns2/adapter/bookclasifier/libraryB/Statistics.java
+++ kodilla-patterns2/src/main/java/com/kodillapatterns2/adapter/bookclasifier/libraryB/Statistics.java
@@ -1,7 +1,8 @@
 package com.kodillapatterns2.adapter.bookclasifier.libraryB;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Statistics implements BookStatistics {
     @Override
@@ -17,18 +18,50 @@ public class Statistics implements BookStatistics {
     @Override
     public int medianPublicationYear(Map<BookSignature, Book> books) {
         if(books.size() == 0) {return 0;}
-        int[]years = new int[books.size()];
-        int n = 0;
-        for(Map.Entry<BookSignature, Book> entry : books.entrySet()) {
-            years[n] = entry.getValue().getYearOfPublication();
-            n++;
-        }
-        Arrays.sort(years);
-        if(years.length % 2 == 0) {
-            return years[(int)(years.length / 2 + 0.5)];
-        } else {
-            return years[years.length/2];
-        }
+
+        /** WEDLE POLECENIA
+         *
+         *         int[]years = new int[books.size()];
+         *         int n = 0;
+         *         for(Map.Entry<BookSignature, Book> entry : books.entrySet()) {
+         *             years[n] = entry.getValue().getYearOfPublication();
+         *             n++;
+         *         }
+         *         Arrays.sort(years);
+         *         if(years.length % 2 == 0) {
+         *             return years[(int)(years.length / 2 + 0.5)];
+         *         } else {
+         *             return years[years.length/2];
+         *         }
+         */
+
+        /** BEZ MODYFIKOWANIA OBIEKTU
+         *
+         *         int bookCount = books.size();
+         *         IntStream sortedYears = books.values().stream()
+         *                                         .mapToInt(Book::getYearOfPublication)
+         *                                         .sorted();
+         *
+         *         int median = bookCount%2 == 0?
+         *                 (int)sortedYears.skip(bookCount/2-1).limit(2).average().getAsDouble():
+         *                 sortedYears.skip(bookCount/2).findFirst().getAsInt();
+         *
+         *         return median;
+         */
+
+        /**
+         * MODYFIKUJĄC OBIEKT
+         */
+
+        List<Book> bookList = books.values().stream().collect(Collectors.toList());
+        bookList.sort(Comparator.comparingInt(Book::getYearOfPublication));
+        int median = bookList.get(bookList.size()/2).getYearOfPublication();
+        if(bookList.size()%2 == 0) median = (median + bookList.get(bookList.size()/2-1).getYearOfPublication()) / 2;
+
+        return median;
+
+
+
 
 
     }
--- kodilla-patterns2/src/test/java/com/kodillapatterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodillapatterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class MedianAdapterTestSuite {
     @Test
-    public void testMedianAdapterPublicationYear() {
+    public void testMedianAdapterPublicationYearOddAmount() {
         //Given
         MedianAdapter adapter = new MedianAdapter();
         Book bookA1 = new Book("abc", "title", 1990, "blabla");
@@ -31,5 +31,27 @@ public class MedianAdapterTestSuite {
 
         //Then
         Assert.assertEquals(1990, result);
+    }
+
+    @Test
+    public void testMedianAdapterPublicationYearEvenAmount() {
+        //Given
+        MedianAdapter adapter = new MedianAdapter();
+        Book bookA1 = new Book("abc", "title", 1990, "blabla");
+        Book bookA2 = new Book("abc", "title", 2010, "blabla");
+        Book bookA3 = new Book("abc", "title", 1957, "blabla");
+        Book bookA4 = new Book("abc", "title", 2012, "blabla");
+
+        Set<Book> bookASet = new HashSet<>();
+        bookASet.add(bookA1);
+        bookASet.add(bookA2);
+        bookASet.add(bookA3);
+        bookASet.add(bookA4);
+
+        //When
+        int result = adapter.publicationYearMedian(bookASet);
+
+        //Then
+        Assert.assertEquals(2000, result);
     }
 }
