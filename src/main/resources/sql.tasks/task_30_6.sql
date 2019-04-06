b6795c27ea969407bf5384e5f7918f63d765449f
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-06 02:42:33 +0200

    correction

--- src/main/resources/sql.tasks/task_30_6.sql
+++ src/main/resources/sql.tasks/task_30_6.sql
@@ -1,5 +1,4 @@
 explain select count(*) from readers where firstname = "John";
-create index firstname_index on readers(firstname);
-create index lastname_index on readers(lastname);
+create index firstname_and_lastname_index on readers(firstname, lastname);
 create index title_index on books(title);
 explain select count(*) from readers where firstname = "John";
