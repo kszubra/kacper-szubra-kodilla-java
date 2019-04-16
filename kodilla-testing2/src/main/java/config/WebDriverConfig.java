0f1720a62dddf5050eccd0a9fc1e39dc2a9e9970
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-17 01:52:47 +0200

    update

--- kodilla-testing2/src/main/java/config/WebDriverConfig.java
+++ kodilla-testing2/src/main/java/config/WebDriverConfig.java
@@ -2,7 +2,7 @@ package config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class WebDriverConfig {
     public final static String CHROME = "CHROME_DRIVER";
