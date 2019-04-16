86a1a0a8552b883b3c49eaed9568e22652471a1e
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-17 00:35:01 +0200

    testing

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/TestLoopAndConditionalBreakpoints.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLoopAndConditionalBreakpoints {
+
+    @Test
+    public void testLoop() {
+        //given
+        long sum = 0;
+        //when
+        for (int n=0; n<1000; n++ ) {
+            sum += n;
+            System.out.println("[" + n + "] sum equals: " + sum);
+        }
+
+        //then
+        Assert.assertEquals(499500, sum);
+    }
+}
