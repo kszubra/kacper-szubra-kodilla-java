0a7ac24529d0f3f345e59eee17a7c1b71159630e
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-04-17 02:15:10 +0200

    update

--- kodilla-testing2/src/main/java/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/google/GoogleTestingApp.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "lst-ib"; //stałą o nazwie SEARCHFIELD, przechowującą identyfikator pola input na stronie, które jest kontrolką służącą do wpisywania tekstu do wyszukania w internecie.
+    public static final String SEARCHFIELD = "gLFyf"; //stałą o nazwie SEARCHFIELD, przechowującą identyfikator pola input na stronie, które jest kontrolką służącą do wpisywania tekstu do wyszukania w internecie.
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME); //tworzymy Selenium WebDriver i otwieramy stronę www.google.com.
@@ -14,6 +14,6 @@ public class GoogleTestingApp {
 
         WebElement searchField = driver.findElement(By.id(SEARCHFIELD)); //wraca ona element typu WebElement na podstawie różnych kryteriów wyszukiwania: identyfikatora, klasy, selektora CSS, wyrażenia xPath (o tym będzie w dalszej części modułu) i innych.
         searchField.sendKeys("Kodilla"); //do znalezionego elementu WebElement wysyłamy zasymulowane przyciśnięcia przycisków klawiatury — wpisujemy w to pole szukaną frazę, czyli wyraz "Kodilla".
-        //searchField.submit();
+        searchField.submit();
     }
 }
